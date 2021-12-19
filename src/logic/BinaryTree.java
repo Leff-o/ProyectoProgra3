ad070e0f27aca80b26f9bec457a6528dadc9b324
Laura Tamayo <laura.tamayo01@uptc.edu.co>
2021-12-19 17:18:36 -0500

    Commit 1

--- src/logic/BinaryTree.java
+++ src/logic/BinaryTree.java
@@ -1,58 +1,119 @@
 package logic;
 
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+/*
++ Peso del árbol
++Número de nodos
+* Altura del nodo
+* Número de enlaces en el camino más largo.
+* Entre ese nodo y una hoja descendente.
++ Nivel de un nodo
++ Distancia del nodo hasta la raiz.
+* Altura o profundidad del arbol
+* Nivel de la hoja del camino más largo
++ Grado de un Nodo
++ Numero de hijos de un nodo
+ */
 
-public class BinaryTree <T>{
-    private Comparator<T> comparator;
+public class BinaryTree<T> {
     private TreeNode<T> root;
+    private Comparator<T> comparator;
     private ArrayList<T> out;
-    private ArrayList<TreeNode<T>> nodesTree;
     private int cont;
 
     public BinaryTree(Comparator<T> comparator) {
         this.comparator = comparator;
-        this.root = null;
+        root = null;
     }
 
-    public boolean isEmpty(){
+    /**
+     * Checks if is empty.
+     *
+     * @return true, if is empty
+     */
+
+    public boolean isEmpty() {
         return root == null;
     }
 
-    public void addNode( T info ){
-        TreeNode<T> node = new TreeNode<>( info );
-        if( isEmpty()){
+    public void addNode(T info) {
+        TreeNode<T> node = new TreeNode<>(info);
+        if (isEmpty()) {
             root = node;
-        }else{
+        } else {
             TreeNode<T> aux = root;
-            TreeNode<T> ant = null;
-            while( aux != null ){
-                ant = aux;
-                aux = comparator.compare(info, aux.getInfo() ) < 0 ? aux.getLeft() : aux.getRight();
+            TreeNode<T> previuos = null;
+            while (aux != null) {
+                previuos = aux;
+                aux = comparator.compare(info, aux.getInfo()) < 0 ? aux.getLeft() : aux.getRight();
             }
-            if( comparator.compare(info,ant.getInfo()) < 0 ){
-                ant.setLeft( node );
-            }else{
-                ant.setRight( node );
+            if (comparator.compare(info, previuos.getInfo()) < 0) {
+                previuos.setLeft(node);
+            } else {
+                previuos.setRight(node);
             }
         }
     }
 
-    public TreeNode<T> findNode( T info ){
-        TreeNode<T> aux = root;
-        while( aux != null ){
-            if( comparator.compare( info, aux.getInfo()) == 0 ){
+    /**
+     * Gets the root.
+     *
+     * @return the root
+     */
+    public TreeNode<T> getRoot() {
+        return root;
+    }
 
+    /**
+     * Adds the node.
+     *
+     * @param info the info
+     */
+    public void addNode2(T info) {
+        root = insert(root, info);
+    }
+
+    /**
+     * Insert.
+     *
+     * @param node the node
+     * @param info the info
+     * @return the tree node
+     */
+    private TreeNode<T> insert(TreeNode<T> node, T info) {
+        if (node == null) {
+            return new TreeNode<T>(info);
+        } else if (comparator.compare((T) node.getInfo(), (T) info) > 0) {
+            node.setLeft(insert(node.getLeft(), info));
+        } else if (comparator.compare((T) node.getInfo(), (T) info) < 0) {
+            node.setRight(insert(node.getRight(), info));
+        } else {
+            throw new RuntimeException("duplicate info!");
+        }
+        return rebalance(node);
+    }
+
+    /**
+     * Find node.
+     *
+     * @param info the info
+     * @return the tree node
+     */
+    public TreeNode<T> findNode(T info) {
+        TreeNode<T> aux = root;
+        while (aux != null) {
+            if (comparator.compare(info, aux.getInfo()) == 0) {
                 return aux;
             }
             aux = comparator.compare(info, aux.getInfo()) < 0 ? aux.getLeft() : aux.getRight();
         }
-
         return null;
     }
 
-    public ArrayList<T> listPresort(){
+    public ArrayList<T> listPresort() {
         out = new ArrayList<>();
         presort(root);
 
@@ -60,14 +121,14 @@ public class BinaryTree <T>{
     }
 
     private void presort(TreeNode<T> root) {
-        if( root != null){
-            out.add( root.getInfo( ) );
-            presort( root.getLeft());
-            presort( root.getRight());
+        if (root != null) {
+            out.add(root.getInfo());
+            presort(root.getLeft());
+            presort(root.getRight());
         }
     }
 
-    public ArrayList<T> listInsort(){
+    public ArrayList<T> listInsort() {
         out = new ArrayList<>();
         insort(root);
 
@@ -75,180 +136,338 @@ public class BinaryTree <T>{
     }
 
     private void insort(TreeNode<T> root) {
-        if( root != null){
-            insort( root.getLeft());
-            out.add( root.getInfo( ) );
-            insort( root.getRight());
+        if (root != null) {
+            insort(root.getLeft());
+            out.add(root.getInfo());
+            insort(root.getRight());
         }
     }
 
-    public ArrayList<T> listAmplitude(){
+    //+ listPosort()
+
+    public ArrayList<T> listPosort() {
         out = new ArrayList<>();
-        ArrayDeque<TreeNode> tail = new ArrayDeque<>();
-        tail.addLast( root );
-        while( !tail.isEmpty( ) ){
-            TreeNode<T> aux = tail.pop();
-            if( aux.getLeft() != null ){
-                tail.addLast( aux.getLeft());
-            }
-            if( aux.getRight() != null ){
-                tail.addLast( aux.getRight());
-            }
-            out.add( aux.getInfo());
-        }
+        posort(root);
 
         return out;
     }
 
-    public boolean isLeaf( TreeNode<T> node ){
+    private void posort(TreeNode<T> root) {
+        if (root != null) {
+            posort(root.getLeft());
+            posort(root.getRight());
+            out.add(root.getInfo());
+        }
+    }
+
+    public ArrayList<T> listAmplitude() {
+        out = new ArrayList<>();
+        ArrayDeque<TreeNode> tail = new ArrayDeque<>();
+        tail.addLast(root);
+        while (!tail.isEmpty()) {
+            TreeNode<T> aux = tail.pop();
+            if (aux.getLeft() != null) {
+                tail.addLast(aux.getLeft());
+            }
+            if (aux.getRight() != null) {
+                tail.addLast(aux.getRight());
+            }
+            out.add(aux.getInfo());
+        }
+        return out;
+    }
+
+    public boolean isLeaf(TreeNode<T> node) {
         return node.getRight() == null && node.getLeft() == null;
     }
 
-    public TreeNode<T> findFather( TreeNode<T> node){
-        if ( node == root ){
+    public TreeNode<T> findFather(TreeNode<T> node) {
+        if (node == root) {
             return null;
-        }else{
+        } else {
             TreeNode<T> aux = root;
-            while( aux.getLeft() != node && aux.getRight() != node ){
+            while (aux.getLeft() != node && aux.getRight() != node) {
                 aux = comparator.compare(node.getInfo(), aux.getInfo()) < 0 ? aux.getLeft() : aux.getRight();
             }
-
             return aux;
         }
     }
 
-    public int levelNode( TreeNode<T> node){
-
-        return node == root ? 0 : levelNode( findFather( node)  ) + 1;
+    public int levelNode(TreeNode<T> node) {
+        return node == root ? 0 : levelNode(findFather(node)) + 1;
     }
 
-    public int heigthNode( TreeNode<T> node){
-        cont = 0;
-        heigth( node, 0 );
-
-        return cont;
+    /**
+     * Height node.
+     *
+     * @param node the node
+     * @return the int
+     */
+    private int heightNode(TreeNode<T> node) {
+        return node == null ? -1 : node.getHeight();
     }
 
     private void heigth(TreeNode<T> node, int i) {
-        if( node != null ){
-            heigth( node.getLeft(), i + 1 );
+        if (node != null) {
+            heigth(node.getLeft(), i + 1);
             cont = i > cont ? i : cont;
-            heigth( node.getRight(), i + 1 );
+            heigth(node.getRight(), i + 1);
         }
     }
 
-    public int heigthTree(){
-        return heigthNode( root );
+    /**
+     * Height tree.
+     *
+     * @return the int
+     */
+    public int heigthTree() {
+        return heightNode(root);
     }
 
-    public int getGradeNode(TreeNode<T> node){
-        if( node.getRight() != null && node.getLeft() != null )
+    public int getGradeNode(TreeNode<T> node) {
+        if (node.getRight() != null && node.getLeft() != null)
             return 2;
-        else if( node.getRight() != null || node.getLeft() != null )
+        else if (node.getRight() != null || node.getLeft() != null)
             return 1;
         else
             return 0;
     }
 
-    public T deleteNode( TreeNode<T> node){
-        switch ( getGradeNode( node ) ){
-            case 0 : return deleteLeaf( node );
+    /**
+     * Update height.
+     *
+     * @param node the node
+     */
+    private void updateHeight(TreeNode<T> node) {
+        node.setHeight(1 + Math.max(heightNode(node.getLeft()), heightNode(node.getRight())));
+    }
 
-            case 1 : return deleteWithSon( node );
+    /**
+     * Rebalance.
+     *
+     * @param node the node
+     * @return the tree node
+     */
+    private TreeNode<T> rebalance(TreeNode<T> node) {
+        updateHeight(node);
+        int balance = getBalance(node);
+        if (balance > 1) {
+            if (heightNode(node.getRight().getRight()) > heightNode(node.getRight().getLeft())) {
+                node = rotateLeft(node);
+            } else {
+                node.setRight(rotateRight(node.getRight()));
+                node = rotateLeft(node);
+            }
+        } else if (balance < -1) {
+            if (heightNode(node.getLeft().getLeft()) > heightNode(node.getLeft().getRight())) {
+                node = rotateRight(node);
+            } else {
+                node.setLeft(rotateLeft(node.getLeft()));
+                node = rotateRight(node);
+            }
+        }
+        return node;
+    }
 
-            default: return deleteWithSons( node );
+    public T deleteNode(TreeNode<T> node) {
+        switch (getGradeNode(node)) {
+            case 0:
+                return deleteLeaf(node);
+            case 1:
+                return deleteWithSon(node);
+
+            default:
+                return deleteWithSons(node);
         }
     }
 
-    private T deleteWithSons(TreeNode<T> node) {
+    /**
+     * Delete node.
+     *
+     * @param info the info
+     */
+    public void deleteNode(T info) {
+        root = delete(root, info);
+    }
 
-        return null;
+    /**
+     * Most left child.
+     *
+     * @param node the node
+     * @return the tree node
+     */
+    private TreeNode<T> mostLeftChild(TreeNode<T> node) {
+        TreeNode<T> current = node;
+        /* loop down to find the leftmost leaf */
+        while (current.getLeft() != null) {
+            current = current.getLeft();
+        }
+        return current;
+    }
+
+    /**
+     * Delete.
+     *
+     * @param node the node
+     * @param info the info
+     * @return the tree node
+     */
+    private TreeNode<T> delete(TreeNode<T> node, T info) {
+        if ((node == null)) {
+            return node;
+        } else if (comparator.compare((T) node.getInfo(), (T) info) > 0) {
+            node.setLeft(delete(node.getLeft(), info));
+        } else if (comparator.compare((T) node.getInfo(), (T) info) < 0) {
+            node.setRight(delete(node.getRight(), info));
+        } else if (node.getLeft() == null || node.getRight() == null) {
+            node = (node.getLeft() == null) ? node.getRight() : node.getLeft();
+        } else {
+            TreeNode<T> mostLeftChild = mostLeftChild(node.getRight());
+            node.setInfo(mostLeftChild.getInfo());
+            node.setRight(delete(node.getRight(), node.getInfo()));
+        }
+
+        if (node != null) {
+            node = rebalance(node);
+        }
+        return node;
+    }
+
+    private T deleteWithSons(TreeNode<T> node) {
+        T out = node.getInfo();
+        if (node == root) {
+            root = root.getLeft() != null ? root.getLeft() : root.getRight();
+        } else {
+            TreeNode<T> father = findFather(node);
+            if (father.getLeft() == node) {
+                father.setLeft(node.getLeft() != null ? node.getLeft() : node.getRight());
+            } else {
+                father.setRight(node.getLeft() != null ? node.getLeft() : node.getRight());
+            }
+        }
+        return out;
+    }
+
+    private T deleteWithSons2(TreeNode<T> node) {
+        T out = node.getInfo();
+        TreeNode<T> sustitute = node.getRight();
+        TreeNode<T> fatherSustitute = null;
+        while (sustitute.getLeft() != null) {
+            fatherSustitute = sustitute;
+            sustitute = sustitute.getLeft();
+        }
+        if (fatherSustitute != null) {
+            fatherSustitute.setLeft(node.getRight());
+            sustitute.setRight(node.getRight());
+        }
+        sustitute.setLeft(node.getLeft());
+
+        //+Buscar padre del nodo o Eliminar
+        TreeNode<T> father = findFather(node);
+        if (father == null) {
+            root = sustitute;
+        } else if (father.getLeft() == node) {
+            father.setLeft(sustitute);
+        } else {
+            father.setRight(sustitute);
+        }
+        return out;
     }
 
     private T deleteWithSon(TreeNode<T> node) {
-
-        return  null;
+        T out = node.getInfo();
+        if (node == root) {
+            root = root.getLeft() != null ? root.getLeft() : root.getRight();
+        } else {
+            TreeNode<T> father = findFather(node);
+            if (father.getLeft() == node) {
+                father.setLeft(node.getLeft() != null ? node.getLeft() : node.getRight());
+            } else {
+                father.setRight(node.getLeft() != null ? node.getLeft() : node.getRight());
+            }
+        }
+        return out;
     }
 
     private T deleteLeaf(TreeNode<T> node) {
         T out = node.getInfo();
-
-        if( node == root ){
+        if (node == root) {
             root = null;
-        }else{
-            TreeNode<T> father = findFather( node );
-            if( father.getLeft() == node ){
-                father.setLeft( null );
-            }else{
-                father.setRight( null );
+        } else {
+            TreeNode<T> father = findFather(node);
+            if (father.getLeft() == node) {
+                father.setLeft(null);
+            } else {
+                father.setRight(null);
             }
         }
 
         return out;
     }
 
-    public int weightTree(){
-        return listInsort().size();
+    public int weightTree() {
+        return weight(root);
     }
 
-    public Object getSustitutes(TreeNode<Integer> findNode) {
-
-        //faltaaaaaaaa
-
-        return null;
-    }
-
-    public Integer countNodesLevel(int i) {
-        nodesTree = new ArrayList<>();
-        presortNode(root);
-        i = 0;
-        for (TreeNode<T> node : nodesTree){
-            if (levelNode(node) == i && isLeaf(node)){
-                i++;
+    /**
+     * Weight.
+     *
+     * @param node the node
+     * @return the int
+     */
+    private int weight(TreeNode<T> node) {
+        int counter = 0;
+        if (node != null) {
+            if (node.getLeft()!=null) {
+                counter += weight(node.getLeft());
+            }
+            counter++;
+            if (node.getRight()!=null) {
+                counter += weight(node.getRight());
             }
         }
-        return cont;
-
+        return counter;
     }
 
-    public int countLeafsLevel(int level){
-
-        nodesTree = new ArrayList<>();
-        presortNode(root);
-        int cont = 0;
-
-        for (TreeNode<T> node : nodesTree){
-            if (levelNode(node) == level && isLeaf(node) == true){
-                cont++;
-            }
-        }
-        return cont;
+    /**
+     * Gets the balance.
+     *
+     * @param node the node
+     * @return the balance
+     */
+    public int getBalance(TreeNode<T> node) {
+        return (node == null) ? 0 : heightNode(node.getRight()) - heightNode(node.getLeft());
     }
 
-    private void presortNode(TreeNode<T> node) {
-        if (node != null){
-            nodesTree.add(node);
-            presortNode(node.getLeft());
-            presortNode(node.getRight());
-        }
+    /**
+     * Rotate right.
+     *
+     * @param y the y
+     * @return the tree node
+     */
+    private TreeNode<T> rotateRight(TreeNode<T> y) {
+        TreeNode<T> x = y.getLeft();
+        TreeNode<T> z = x.getRight();
+        x.setRight(y);
+        y.setLeft(z);
+        updateHeight(y);
+        updateHeight(x);
+        return x;
     }
 
-    public Integer weightNode(TreeNode<Integer> findNode) {
-
-        if (findNode != null){
-            return (weightNode(findNode.getLeft()))+(weightNode(findNode.getRight()))+1;
-        }
-        return 0;
+    /**
+     * Rotate left.
+     *
+     * @param y the y
+     * @return the tree node
+     */
+    private TreeNode<T> rotateLeft(TreeNode<T> y) {
+        TreeNode<T> x = y.getRight();
+        TreeNode<T> z = x.getLeft();
+        x.setLeft(y);
+        y.setRight(z);
+        updateHeight(y);
+        updateHeight(x);
+        return x;
     }
-
-    public double avgBranchHeihgts(TreeNode<T> findNode) {
-        if (!isLeaf(findNode)) {
-            double left = findNode.getLeft() != null ? heigthNode(findNode.getLeft()) + 1 : 0;
-            double right = findNode.getRight() != null ? heigthNode(findNode.getRight()) + 1 : 0;
-            return ((left + right) / 2);
-        }
-        return 0;
-    }
-
-
 }
--- src/logic/TreeNode.java
+++ src/logic/TreeNode.java
@@ -1,17 +1,16 @@
 package logic;
 
 public class TreeNode <T>{
-
     private T info;
-
+    private int height;
     private TreeNode<T> left;
-
     private TreeNode<T> right;
 
     public TreeNode(T info) {
         this.info = info;
-        this.left = null;
-        this.right = null;
+        height = 0;
+        left = null;
+        right = null;
     }
 
     public T getInfo() {
@@ -36,5 +35,13 @@ public class TreeNode <T>{
 
     public void setRight(TreeNode<T> right) {
         this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
